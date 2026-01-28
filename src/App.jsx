d01e7d04fa0f222e445d35de9fefa1718b97a3cf
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2026-01-28 07:34:14 -0300

    Projeto Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import {
+  faBars,
+  faSearch,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className="nav">
+          <div className="inner-content">
+            <h1 className="logo">
+              Bit<span>Buy</span>
+            </h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/products">Produtos</Link>
+                </li>
+                <li>
+                  <Link to="/about">Sobre</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contato</Link>
+                </li>
+                <li>
+                  <Link to="/account">Conta</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="navs-icon-container">
+              <div className="search-input-container">
+                <input type="search" placeholder="Procurar" />
+                <FontAwesomeIcon icon={faSearch} />
+              </div>
+              <button className="shopping-cart">
+                <FontAwesomeIcon icon={faShoppingCart} />
+                <div className="products-count">0</div>
+              </button>
+              <button className="menu-button">
+                <FontAwesomeIcon icon={faBars} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
